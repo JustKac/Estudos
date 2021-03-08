a284644bdd7ccd45630c04759b80dd10e121ab19
Caio Henrique Cordeiro Lins <caiohrick@outlook.com>
2021-03-08 19:59:23 -0300

    Primeiro projeto da Avanade Angular Developer

---
+++ bootcamp_avanade_developer/projeto_avanade_dio_1/js/main.js
@@ -0,0 +1,8 @@
+
+function openwiki(){
+    window.open("https://pt.wikipedia.org/wiki/Arquitetura")  //Abre o site em outra aba
+};
+
+function openarchdaily(){
+    window.open("https://www.archdaily.com.br/br")  //Abre o site em outra aba
+};
